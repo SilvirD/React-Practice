a038eddcce6faff6f4473482bc26095a76044488
Tran Trong Duc <trtrongduc2212@gmail.com>
2021-08-29 22:29:56 +0700

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.scss";
-import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome to React Hooks</h1>
-      <ColorBox />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TodoList.scss";
+
+TodoList.propTypes = {};
+
+function TodoList(props) {
+  return <div></div>;
+}
+
+export default TodoList;
