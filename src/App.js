ca1b0e620b4e421163008b5386012a39908bd3f4
Tran Trong Duc <bupbi99@gmail.com>
2021-09-06 17:36:53 +0700

    Learn basic useEffect and Pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
+import Pagination from "./components/Pagination";
+import PostList from "./components/PostList";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+
+import queryString from "query-string";
 
 const TODO_LIST = [
   { id: 1, title: "I learn ReactJS" },
@@ -11,6 +15,50 @@ const TODO_LIST = [
 
 function App() {
   const [todoList, setTodoList] = useState(TODO_LIST);
+
+  const [postList, setPostList] = useState([]);
+
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  });
+
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1,
+  });
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const paramsString = queryString.stringify(filters);
+        const requestURL = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
+
+        const response = await fetch(requestURL);
+        const responseJSON = await response.json();
+
+        console.log(responseJSON);
+
+        const { data, pagination } = responseJSON;
+
+        setPostList(data);
+        setPagination(pagination);
+      } catch (error) {
+        console.log("Failed to fetch post list: ", error.message);
+      }
+    }
+
+    fetchPostList();
+  }, [filters]);
+
+  function handlePageChange(newPage) {
+    console.log("New Page: ", newPage);
+    setFilters({
+      ...filters,
+      _page: newPage,
+    });
+  }
 
   function handleTodoClick(todo) {
     console.log(todo);
@@ -38,9 +86,14 @@ function App() {
 
   return (
     <div className="app">
-      <h1>React Hooks - Todo List</h1>
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <h1>React Hooks - Post List</h1>
+
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+
+      <PostList posts={postList} />
+
+      <Pagination pagination={pagination} onPageChange={handlePageChange} />
     </div>
   );
 }
